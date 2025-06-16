2acdda02d54d06d965dd086e8fa8f6269473b0bd
Pietro Mambelli <pietro.mambelli@cashmatic.it>
2025-06-16 17:37:28 +0200

    aggiunto monitoraggio download status

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, request, jsonify, render_template
-from logic import load_playlist
-from logic import download_url_list
+from flask import Flask, request, Response, jsonify, render_template
 import webbrowser
 from flask import send_from_directory
 import os
-from threading import Timer
+import time
+import yt_dlp
+import threading
+import re
+
+
+# from logic import load_playlist, download_url, progress_data
 
 app = Flask(__name__)
 
@@ -23,9 +27,14 @@ def load_data():
     result = load_playlist(url)
     return jsonify(result)
 
+def handle_download(item, format, dest_folder):
+    print("Started download for " + str(item['id']))
+    progress_data[item['id']] = '0.0%'
+    download_url(item, format, dest_folder)
 
 @app.route('/download', methods=['POST'])
 def download_urls():
+    threads = []
     data = request.json
     urls = data.get('urls')
     
@@ -35,13 +44,106 @@ def download_urls():
         return {'status': 'error', 'message': 'Missing URLs or format.'}, 400
 
     dest_folder = './downloads'
+    
+    for item in urls:
+        t = threading.Thread(target=handle_download, args=(item, format, dest_folder))
+        t.start()
+        threads.append(t)
+        
+    return {'status': 'success', 'message': 'Download started.'}, 200
 
-    return download_url_list(urls, format, dest_folder), 200
 
+@app.route('/progress/<id>')
+def progress_stream(id):
+    def generate():
+        while True:
+            if(progress_data is None or not progress_data):
+                pass
+            
+            percentage = str(progress_data[str(id)])
+            yield f"data: {percentage}\n\n"
+            if percentage == '100.0%' or percentage == "FAILED":
+                break
+            time.sleep(0.2)
+    return Response(generate(), mimetype='text/event-stream')
 
 def open_browser():
       webbrowser.open_new("http://127.0.0.1:5000")
+      
+
+def load_playlist(playlist_url):
+    ydl_opts = {
+        'quiet': True,
+        'extract_flat': True,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        playlist_dict = ydl.extract_info(playlist_url, download=False)
+
+    urls = list()
+
+    for video in playlist_dict['entries']:
+        urls.append({
+            'url' : video['url'],
+            'title' : video['title']
+        })
+
+    print(f"Number of videos loaded from playlist: {len(urls)}")
+    
+    return urls
+    
+progress_data = {}
+counter_lock = threading.Lock()
+def create_hook(item):  
+    def progress_hook(d):
+        if d['status'] == 'downloading':
+            percentage = d.get('_percent_str', '0.0%')
+            ansi_escape = re.compile(r'\x1b\[[0-9;]*m')
+            clean_text = ansi_escape.sub('', percentage)
+            
+            progress_data[item['id']] = clean_text
+    return progress_hook
+
+def download_url(item, format, dest_folder):
+    # item:
+    #     id
+    #     url
+    #     title
+    hook = create_hook(item)
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'quiet' : True,
+        'progress_hooks': [hook],
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': format,
+            'preferredquality': '0',
+        }],
+        'outtmpl': dest_folder+'/%(title)s.%(ext)s',
+    }
+    
+    success_counter = 0
+    fail_counter = 0
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        try:
+            with counter_lock:
+                progress_data[item['id']] = '0.0%'
+                dl_result = ydl.download(item['url'])
+            
+                if(dl_result == 0):
+                    success_counter+=1
+                    progress_data[item['id']] = '100.0%'
+                else:
+                    fail_counter+=1
+                    progress_data[item['id']] = 'FAILED'
+                
+        except Exception as e:
+            with counter_lock:
+                fail_counter+=1
+            print(f"Failed to download: {e}")
+            pass
 
 if __name__ == "__main__":
-    Timer(1, open_browser).start()
+    # Timer(1, open_browser).start()
     app.run(debug=True)
--- logic.py
+++ logic.py
@@ -1,4 +1,6 @@
 import yt_dlp
+import threading
+import re
 #Install yt_dlp using:      pip.exe install git+https://github.com/nficano/pytube
 
 # playlist_url = "https://www.youtube.com/playlist?list=PLSNrCkxRaAmqvzm3TnSEQx-eJdLSjwnIS" #groove
@@ -28,25 +30,28 @@ def load_playlist(playlist_url):
     
     return urls
     
+progress_data = {}
+counter_lock = threading.Lock()
+def create_hook(item):  
+    def progress_hook(d):
+        if d['status'] == 'downloading':
+            percentage = d.get('_percent_str', '0.0%')
+            ansi_escape = re.compile(r'\x1b\[[0-9;]*m')
+            clean_text = ansi_escape.sub('', percentage)
+            
+            progress_data[item['id']] = clean_text
+    return progress_hook
 
-# dest_folder = input("Name the folder where the files will be saved:")
-
-# valid_formats = ["flac", "mp3", "wav"]
-# selected_format = ""
-
-# while True:
-#     user_input = input("Please enter a format (flac, mp3, wav): ").lower()
-#     if user_input in valid_formats:
-#         selected_format = user_input
-#         break
-#     else:
-#         print("Invalid input. Please try again.")
-
-
-def download_url_list(urls, format, dest_folder):
+def download_url(item, format, dest_folder):
+    # item:
+    #     id
+    #     url
+    #     title
+    hook = create_hook(item)
     ydl_opts = {
         'format': 'bestaudio/best',
         'quiet' : True,
+        'progress_hooks': [hook],
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
             'preferredcodec': format,
@@ -59,20 +64,64 @@ def download_url_list(urls, format, dest_folder):
     fail_counter = 0
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        for url in urls:
-            print(url)
-            try:
-                print(f"Downloading: {url['title']}")
-                dl_result = ydl.download(url['url'])
-                
+        try:
+            with counter_lock:
+                progress_data[item['id']] = 'Starting...'
+                dl_result = ydl.download(item['url'])
+            
                 if(dl_result == 0):
                     success_counter+=1
+                    progress_data[item['id']] = '100.0%'
                 else:
                     fail_counter+=1
-                    
-            except Exception as e:
+                    progress_data[item['id']] = 'FAILED'
+                
+        except Exception as e:
+            with counter_lock:
                 fail_counter+=1
-                print(f"Failed to download: {e}")
-                pass
+            print(f"Failed to download: {e}")
+            pass           
+
+# def download_url_list(urls, format, dest_folder):
+#     def progress_hook(d):
+#         if d['status'] == 'downloading':
+#             percentage = d.get('_percent_str', '0.0%')
+#             progress_data[int(urls['id'])] = percentage
+#             print("Updated data")
+
+#     ydl_opts = {
+#         'format': 'bestaudio/best',
+#         'quiet' : True,
+#         'progress_hooks': [progress_hook],
+#         'postprocessors': [{
+#             'key': 'FFmpegExtractAudio',
+#             'preferredcodec': format,
+#             'preferredquality': '0',
+#         }],
+#         'outtmpl': dest_folder+'/%(title)s.%(ext)s',
+#     }
+    
+#     success_counter = 0
+#     fail_counter = 0
+
+#     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+#         for url in urls:
+#             try:
+#                 progress_data[int(urls['id'])] = 'STARTING...'
+#                 print(f"Downloading: {url['title']}")
+#                 dl_result = ydl.download(url['url'])
+                
+#                 if(dl_result == 0):
+#                     success_counter+=1
+#                 else:
+#                     progress_data[int(urls['id'])] = 'FAILED'
+#                     fail_counter+=1
+                    
+#             except Exception as e:
+#                 fail_counter+=1
+#                 print(f"Failed to download: {e}")
+#                 pass
             
-        return "Download finished. "+ str(success_counter)+ " downloaded. "+ str(fail_counter) + " failed."
+#             progress_data[int(urls['id'])] = '100.0%'
+        
+#         return "Download finished. "+ str(success_counter)+ " downloaded. "+ str(fail_counter) + " failed."
